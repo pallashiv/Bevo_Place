5157be4caf63e142d7451a19649559f1df80435a
pallashiv <pallashivsagar@gmail.com>
2025-07-28 17:30:32 -0500

    redo

---
+++ BevoPlace/HelpViewController.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BevoPlace/WishListProductCell.swift
@@ -0,0 +1,16 @@
+
+
+import Foundation
+import UIKit
+
+class WishListProductCell: UITableViewCell {
+    
+    //Outlets
+    @IBOutlet weak var wishProductImage: UIImageView!
+    @IBOutlet weak var wishProductTitleLabel: UILabel!
+    @IBOutlet weak var wishProductCategoryLabel: UILabel!
+    @IBOutlet weak var wishProductPriceLabel: UILabel!
+    @IBOutlet weak var wishLeaseLengthLabel: UILabel!
+    @IBOutlet weak var wishLeaseBuyLabel: UILabel!
+    @IBOutlet weak var wishDummyLeaseLengthLabel: UILabel!
+}
